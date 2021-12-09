0b03b17ef6268cb8e401457e0d3d07446e5da576
Daniel Bubenheim <daniel.bubenheim@gmail.com>
2021-12-09 22:08:51 +0100

    feat: adds tests for day 06
    Signed-off-by: Daniel Bubenheim <daniel.bubenheim@gmail.com>

--- app/src/test/kotlin/aoc2021/Day06Test.kt
+++ app/src/test/kotlin/aoc2021/Day06Test.kt
@@ -1,6 +1,7 @@
 package aoc2021
 
-import aoc2021.day06.Day06
+import aoc2021.day06.Day06.Companion.lanternfishPart1
+import aoc2021.day06.Day06.Companion.lanternfishPart2
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.Test
@@ -8,5 +9,10 @@ import org.junit.jupiter.api.Test
 class Day06Test {
 
     @Test
-    fun testLanternfishPart1() = assertThat(Day06.lanternfishPart1()).isEqualTo(5934)
+    fun testLanternfishPart1() =
+        assertThat(lanternfishPart1()).isEqualTo(353079)
+
+    @Test
+    fun testLanternfishPart2() =
+        assertThat(lanternfishPart2()).isEqualTo(1605400130036)
 }
